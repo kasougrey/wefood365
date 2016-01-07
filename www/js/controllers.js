5c4fae8406cf398e5e244cffc1f20b3c1047976b
kasougrey <6335684@qq.com>
2016-01-07 23:52:41 +0800

    解决了路由问题(ui-route)

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,18 +3,14 @@ angular.module('wefood.controllers', [])
 .controller('ServiceCtrl', function($scope) {
 
 })
+.controller('TestCtrl', function($scope) {
+  console.log($scope);
+})
 .controller('ServiceDishCtrl', function($scope,$ionicHistory) {
 
 
 })
-.controller('ServiceDishConfirmCtrl', function($scope,$ionicLoading) {
-  $scope.firstNum = 0;
-  $scope.test = function(a) {
-    $scope.firstNum ++;
-    console.log($scope.firstNum);
-  }
-
-
+.controller('ServiceDishConfirmCtrl', function($scope) {
 
 })
 .controller('EventCtrl', function($scope) {
@@ -24,6 +20,11 @@ angular.module('wefood.controllers', [])
 .controller('CartCtrl', function($scope) {
 
 })
-.controller('AccountCtrl', function($scope) {
-
+.controller('AccountCtrl', function($scope,$state) {
+  $scope.goUrl = function(url) {
+    $state.go(url);
+  }
+})
+.controller('AccountOrderListCtrl',function($scope) {
+  //我的订单
 });
